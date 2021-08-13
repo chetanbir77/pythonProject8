113cdd604a8f750bc27aed4916677533cf2a473c
chetanbir77 <chetanbir.s@felixhealthcare.ai>
2021-08-13 12:12:49 +0530

    com

--- homelane/wsgi.py
+++ homelane/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
@@ -15,3 +16,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homelane.settings')
 
 application = get_wsgi_application()
+path = '/home/chetanbir77/pythonProject8'
+if path not in sys.path:
+    sys.path.insert(0, path)
